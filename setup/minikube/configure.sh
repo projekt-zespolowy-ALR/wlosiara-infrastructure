bf4dfa425a22311bf2a0b89935420254b027ada3
Rafał Majewski <goodheropl@gmail.com>
2023-06-09 00:09:59 +0200

    Add driver

--- setup/minikube/configure.sh
+++ setup/minikube/configure.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-minikube start --disk-size='5000mb' --cpus=3 --memory='3096mb'
+minikube start --disk-size='5000mb' --cpus=3 --memory='3096mb' --driver='virtualbox'
 minikube ip
 minikube addons enable ingress
 kubectl get pods -n ingress-nginx
